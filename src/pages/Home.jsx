2a145ea4249369936c4b912e2222e489f20ef298
abuNaafi <abdisalanyusuf10@gmail.com>
2022-12-26 11:48:03 +0000

    update

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import UserResults from "../components/users/UserResults";
+import UserSearchh from "../components/users/UserSearchh";
+
+function Home() {
+  return (
+    <>
+   <UserSearchh/>
+   <UserResults/>
+    </>
+  );
+}
+
+export default Home;
